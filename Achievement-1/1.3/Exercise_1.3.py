35ee471cc34757e1749676316b6deea8c95a01df
Philippe <ducassephi@hotmail.fr>
2023-10-19 20:03:51 +0200

    exercise 1.3

--- Achievement-1/1.3/Exercise_1.3.py
+++ Achievement-1/1.3/Exercise_1.3.py
@@ -1,5 +1,5 @@
 recipes_list = []
-ingredients_list = set()
+ingredients_list = []
 
 n = int(input("how many recipes would you like to define?"))
 
@@ -7,7 +7,7 @@ n = int(input("how many recipes would you like to define?"))
 def take_recipe(*recipe):
     name = input("Enter recipe name:")
     cooking_time = int(input("Enter cooking time in minutes:"))
-    ingredients = list(input("Enter ingredients list:").split())
+    ingredients = list(input("Enter ingredients seperated by a comma and space:").lower().split(", "))
     recipe = {"name": name, "cooking_time": cooking_time, "ingredients": ingredients}
     return recipe
 
@@ -15,33 +15,31 @@ def take_recipe(*recipe):
 for recipe in range(0, n):
     recipe = take_recipe()
     recipes_list.append(recipe)
-    # ingredients = [ ingredient for ingredient in recipe['ingredients'] if ingredient not in ingredients_list]
     for ingredient in recipe["ingredients"]:
         if not ingredient in ingredients_list:
-            ingredients_list.add(ingredient)
+            ingredients_list.append(ingredient)
 
 for recipe in recipes_list:
-    if recipe["cooking_time"] < 10 and len(recipe["ingredients"]) < 4:
+    cooking_time = recipe["cooking_time"]
+    ing_length = len(recipe["ingredients"])
+
+    if cooking_time < 10 and ing_length < 4:
         difficulty = "Easy"
-    if recipe["cooking_time"] < 10 and len(recipe["ingredients"]) >= 4:
+    if cooking_time < 10 and ing_length >= 4:
         difficulty = "Medium"
-    if recipe["cooking_time"] >= 10 and len(recipe["ingredients"]) < 4:
+    if cooking_time >= 10 and ing_length < 4:
         difficulty = "Intermediate"
-    if recipe["cooking_time"] >= 10 and len(recipe["ingredients"]) >= 4:
+    if cooking_time >= 10 and ing_length >= 4:
         difficulty = "Hard"
     print(
-        "recipe :",
-        recipe["name"],
-        "\n",
-        "cooking time (min): ",
-        recipe["cooking_time"],
-        "\n",
-        "ingredients: ",
-        " ".join(recipe["ingredients"]),
-        "\n",
-        "difficulty level: ",
-        difficulty,
+        f"""recipe : {recipe["name"]}
+        cooking time (min): {cooking_time}
+        ingredients: {" ".join(recipe["ingredients"])}
+        difficulty level: {difficulty}"""
     )
 
 sorted_ingredients = sorted(ingredients_list)
-print("ingredients available across all recipes: ", " ".join(sorted_ingredients))
+
+print("Ingredients available across all recipes: ")
+for ing in sorted_ingredients:
+    print(ing)
